33a7bc90aa0fc0c4fa6d6d2a9ffd0681721239e2
Ivan Stepanuyk <i.stepanuyk@hedgehog.technology>
2017-12-15 11:29:55 +0200

    hercules-poirot-and-squares is works !!!! :)

--- homeworks/1/hercules-poirot-and-squares.js
+++ homeworks/1/hercules-poirot-and-squares.js
@@ -1,5 +1,5 @@
 
-var str = "3\n3 9\n17 24\n25 100";
+var str = "3\n1 16\n3 4\n100 100";
 // console.log(str);
 processData(str);
 
@@ -10,18 +10,15 @@ function processData(input) {
 
   for(var i = 1; i <= t; i++)
   {
-  	let k = 0;
   	let a =  Number.parseInt(input.split('\n')[i].split(' ')[0]);
   	let b =  Number.parseInt(input.split('\n')[i].split(' ')[1]);
-
-  	// console.log(`${a}=`+Math.ceil((Math.sqrt(a))));
-  	// console.log(`${b}=`+Math.floor((Math.sqrt(b))));
-  	if (Math.sqrt(a) % 1 === 0) { k++; }
-  	if (Math.sqrt(b) % 1 === 0 && Math.sqrt(a) !== Math.sqrt(b)) { k++; }
-  	let r = Math.floor((Math.sqrt(b)))- Math.floor((Math.sqrt(a))) + k;
-  	if (r < 0) r = 0;
+    let r = Math.floor((Math.sqrt(b))) - Math.ceil((Math.sqrt(a)));
+    
+    if (r < 0) 
+      r = 0;
+    else
+      r++;
   	result.push(r);
-
   }
   console.log(result.join('\n'));
 
