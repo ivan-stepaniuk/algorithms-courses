7f9f419db719b064cae6a360ca3f560f39baaff4
ivan-stepaniuk <Stepanuyk.i@gmail.com>
2017-12-15 23:24:44 +0200

    hzhz test #10 dont pass

--- homeworks/1/playing-with-the-alphabet.js
+++ homeworks/1/playing-with-the-alphabet.js
@@ -10,18 +10,23 @@
 // ------
 // YES
 
-var str = "h\nh\n9";
+
+
+
+
+
+
+var str = "aba333\naba333\n6";
 console.log(str);
 processData(str);
 
 function processData(input) {
 
-  let s = input.split('\n')[0].trim();
-  let t = input.split('\n')[1].trim();
+  let s = input.split('\n')[0];
+  let t = input.split('\n')[1];
   let k = input.split('\n')[2];
 
-  var startCount = s.length - 1;
-  if(s == t) {console.log('Yes'); return;}
+  var startCount = s.length -1;
 
   for(var i = 0; i < s.length; i++)
   {
@@ -29,9 +34,11 @@ function processData(input) {
   }
   var del = s.length - startCount;
   var append = t.length - startCount;
-  if (del + append > k) 
+  console.log(append)
+  console.log(del)
+
+  if ((del + append > k) || ( (s.length > k ) &&((k-del-append) % 2 != 0)) ) 
     console.log('No');
   else
     console.log('Yes');
-
 }
