b86f52fba518ddecbcd682454cc730389eba9ee7
ivan-stepaniuk <Stepanuyk.i@gmail.com>
2017-12-17 13:46:27 +0200

    Hercule Poirot and Arrays - done

--- homeworks/1/hercule-poirot-and-arrays.js
+++ homeworks/1/hercule-poirot-and-arrays.js
@@ -1,7 +1,18 @@
-
-var str = "2\n5\n0 0 0 0 1\n5\n0 0 0 0 0";
-// console.log(str);
-processData(str);
+console.log(comparing(processData("1\n3\n1 2 3"), 'NO'));
+console.log(comparing(processData("1\n5\n0 0 0 0 1"), 'YES'));
+console.log(comparing(processData("1\n4\n1 2 3 3"), 'YES'));
+console.log(comparing(processData("1\n4\n0 0 0 0"), 'YES'))
+console.log(comparing(processData("1\n4\n0 0 0 1"), 'YES'));
+console.log(comparing(processData("1\n4\n1 0 0 1"), 'YES'));
+console.log(comparing(processData("1\n4\n0 0 1 1"), 'NO'));
+console.log(comparing(processData("1\n4\n0 0 0 1"), 'YES'));
+console.log(comparing(processData("1\n2\n0 1"), 'YES'));
+console.log(comparing(processData("1\n2\n0 0"), 'YES'));
+console.log(comparing(processData("1\n2\n2 1"), 'NO'));
+console.log(comparing(processData("1\n4\n9 7 8 1"), 'YES'));
+console.log(comparing(processData("1\n11\n5 5 5 5 5 5 5 5 5 5 5"), 'YES'));
+console.log(comparing(processData("1\n11\n1 1 0 0 0 0 0 0 0 1 1"), 'YES'));
+console.log(comparing(processData("1\n1\n1"), 'YES'));
 
 function processData(input) {
 
@@ -11,10 +22,12 @@ function processData(input) {
   for(var i = 1; i <= t*2; i+=2)
   {
 
-  	let n =  Number.parseInt(input.split('\n')[i].split(' ')[0]);
+    let n =  Number.parseInt(input.split('\n')[i].split(' ')[0]);
+    if (n == 1) {
+      result.push('YES'); continue;
+    }
     let a =  input.split('\n')[i+1].split(' ');
-    // console.log(n);
-    // console.log(A);
+
     let sum = 0;
     for(var j = 0; j < n; j++)
       sum += +a[j];
@@ -24,7 +37,6 @@ function processData(input) {
     let res = 'NO';
 
     for(var j = 1; j <= n; j++) {
-      console.log(`${sumL}-${sumR}`);
       if(sumL == sumR - +a[j]) { 
         res = 'YES'; 
         break; 
@@ -36,8 +48,13 @@ function processData(input) {
 
     }
     result.push(res);
-    console.log('-----------');
+    // console.log('-----------');
    }
-  console.log(result.join('\n'));
+  return (result.join('\n'));
 
+}
+
+function comparing(a,b) {
+  if (a===b) return true;
+  return false;
 }
