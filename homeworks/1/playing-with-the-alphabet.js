f2f917be639d51ba3e033fb5cf6768996c411aec
ivan-stepaniuk <Stepanuyk.i@gmail.com>
2017-12-17 13:17:58 +0200

    trying fix bug, not working yet :(

--- homeworks/1/playing-with-the-alphabet.js
+++ homeworks/1/playing-with-the-alphabet.js
@@ -1,32 +1,31 @@
-// hackerhappy
-// hackerrank
-// 9
-// -------
-// YES
+var str = "aba\naba\n3";
+var str1 = "a\naba333\n3";
+var str2= "ab\naba333\n3";
+var str3 = "ab3\naba333\n3";
+var str4 = "a333\naba333\n3";
+var str5 = "ab\naba333\n10";
+var str6 = "ab3\naba333\n11";
+var str7 = "a333\naba333\n11";
 
-// aba
-// aba
-// 7
-// ------
-// YES
-
-
-
-
-
-
-
-var str = "aba333\naba333\n6";
 console.log(str);
 processData(str);
 
+// processData(str1);
+// processData(str2);
+// processData(str3);
+// processData(str4);
+// processData(str5);
+// processData(str6);
+// processData(str7);
 function processData(input) {
 
   let s = input.split('\n')[0];
   let t = input.split('\n')[1];
   let k = input.split('\n')[2];
-
-  var startCount = s.length -1;
+console.log(s);
+console.log(t);
+console.log(k);
+  var startCount = s.length - 1;
 
   for(var i = 0; i < s.length; i++)
   {
@@ -37,8 +36,9 @@ function processData(input) {
   console.log(append)
   console.log(del)
 
-  if ((del + append > k) || ( (s.length > k ) &&((k-del-append) % 2 != 0)) ) 
+  if ((del + append > k) ) 
     console.log('No');
   else
     console.log('Yes');
+    console.log('------')
 }
