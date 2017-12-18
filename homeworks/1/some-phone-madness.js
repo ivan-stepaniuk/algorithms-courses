251305b82bec80365e003c7448aafe5c581a5d7d
Ivan Stepanuyk <i.stepanuyk@hedgehog.technology>
2017-12-18 16:44:47 +0200

    work with O(n2)

--- homeworks/1/some-phone-madness.js
+++ homeworks/1/some-phone-madness.js
@@ -1,7 +1,7 @@
 
-var str = "2\n5\n1 4 3 2 5\n5\n3 4 5 6 7 8 9 10 11 12 13 14 1 2";
-// console.log(str);
-processData(str);
+// var str = "2\n5\n1 4 3 2 5\n5\n3 4 5 6 7 8 9 10 11 12 13 14 1 2";
+// // console.log(str);
+// processData(str);
 
 function processData(input) {
 
@@ -12,34 +12,59 @@ function processData(input) {
     {
 
         let n =  Number.parseInt(input.split('\n')[i].split(' ')[0]);
-        let a =  input.split('\n')[i+1].split(' ');
 
-        let sum = 0;
-        let res = 0;
-        
-        for (var j = 0; j < a.length; j++) {
-            let steps = Math.abs(a[j] - (j+1));
-            if (steps > 2) { res = -1; break;}
+        let a =  input.split('\n')[i+1].split(' ');
+        let aCounts = [];
+        let k = 0;
+        let exit = false;
+
+        for(var j = 0; j < n; j++){
+           aCounts[j] = 0;
+           a[j] = Number.parseInt(a[j]);
+        }
+        // console.log(a);
+      for (var q = n-1; q > 0; q--) {
+        for (var j = n-1; j > 0; j--) {
+          if( a[j] < a[j-1]){
+            aCounts[j-1]++;
+
+            let tmp = a[j];
+            a[j] = a[j-1];
+            a[j-1] = tmp;
             
-            sum += steps;
+            tmp = aCounts[j];
+            aCounts[j] = aCounts[j-1];
+            aCounts[j-1] = tmp;
+
+            k++;
+            if (aCounts[j] > 2){ 
+              // console.log('Too chaotic')
+              exit = true;
+              k = ('Too chaotic');
+              break;
+            }
+            if(exit) break;
+          }
+          // console.log(aCounts);
+          // console.log(a);
+          // console.log(k);
         }
-        if (res == -1) {
-            result.push('Too chaotic');
-        }
-        else
-        result.push(sum/2);
     }
+    result.push(k);
+  }
     console.log(result.join('\n'));
+    return (result.join(' '));
 }
 
-// 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
-// 2 1 5 3 4
-// 1 1 2 1 1 = 3
+function comparing(a,b) {
+  if (a===b) return true;
+  return false;
+}
 
-
-// 2 5 1 3 4
-// 2 1 5 3 4
-// 1 2 5 3 4
-
-12345
-231
+// 
+console.log(comparing(processData('1\n5\n2 1 5 3 4'),'3'));
+console.log(comparing(processData('1\n5\n2 5 1 3 4'),'Too chaotic'));
+console.log(comparing(processData('1\n10\n1 2 5 6 4 7 9 8 3 10'),'9'));
+console.log(comparing(processData('1\n7\n1 2 4 6 5 7 3'),'5'));
+console.log(comparing(processData('1\n14\n3 4 5 6 7 8 9 10 11 12 13 14 1 2'),'24'));
+console.log(comparing(processData('1\n5\n1, 4, 3, 2, 5'),'3'));
