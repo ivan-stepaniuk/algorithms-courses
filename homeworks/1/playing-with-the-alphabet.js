e9b661b1276682e691a27e828ce3b899bff12187
Ivan Stepanuyk <i.stepanuyk@hedgehog.technology>
2017-12-18 12:14:39 +0200

    playing-with-the-alphabet is work :)

--- homeworks/1/playing-with-the-alphabet.js
+++ homeworks/1/playing-with-the-alphabet.js
@@ -1,44 +1,42 @@
-var str = "aba\naba\n3";
-var str1 = "a\naba333\n3";
-var str2= "ab\naba333\n3";
-var str3 = "ab3\naba333\n3";
-var str4 = "a333\naba333\n3";
-var str5 = "ab\naba333\n10";
-var str6 = "ab3\naba333\n11";
-var str7 = "a333\naba333\n11";
 
-console.log(str);
-processData(str);
-
-// processData(str1);
-// processData(str2);
-// processData(str3);
-// processData(str4);
-// processData(str5);
-// processData(str6);
-// processData(str7);
 function processData(input) {
-
   let s = input.split('\n')[0];
   let t = input.split('\n')[1];
   let k = input.split('\n')[2];
-console.log(s);
-console.log(t);
-console.log(k);
-  var startCount = s.length - 1;
+  let startCount = s.length;
+  if((s.length + t.length) <= k)
+    return 'Yes';
 
   for(var i = 0; i < s.length; i++)
   {
     if (s[i] != t[i]) { startCount = i; break;}
   }
-  var del = s.length - startCount;
-  var append = t.length - startCount;
-  console.log(append)
-  console.log(del)
+  let del = s.length - startCount;
+  let append = t.length - startCount;
 
-  if ((del + append > k) ) 
-    console.log('No');
+  if ((del + append <= k) && (append + del - k) % 2 == 0 ) 
+    return ('Yes');
   else
-    console.log('Yes');
-    console.log('------')
+    return ('No');
 }
+
+
+
+function comparing(a,b) {
+  if (a===b) return true;
+  return false;
+}
+
+
+console.log(comparing(processData('hackerhappy\nhackerrank\n9'),'Yes'));
+console.log(comparing(processData('aba\naba\n7'),'Yes'));
+console.log(comparing(processData('happynewyear\nhappyeaster\n13'),'Yes'));
+console.log(comparing(processData('abcd\nabdc\n3'),'No'));
+console.log(comparing(processData('a333\naba333\n3'),'No'));
+console.log(comparing(processData('ab\naba333\n10'),'Yes'));
+console.log(comparing(processData('ab3\naba333\n11'),'Yes'));
+// //
+console.log(comparing(processData('aba33\naba333\n3'),'Yes'));
+console.log(comparing(processData('aba\naba\n3'), 'No'));
+//
+console.log(comparing(processData('hackerhappy\nhackerrank\n9'),'Yes'));
